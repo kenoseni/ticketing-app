6f095ccd8ba4de7b22a6296c20a77e0dde7f39e8
Olusola Oseni <kenolusola@gmail.com>
2022-01-11 12:44:54 +0100

    add ingress service configuration file

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,13 @@
-import express from 'express';
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
+
+app.get("/api/users/currentuser", (req, res) => {
+  res.json("hi there");
+});
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000')
-})
+  console.log("Listening on port 3000!");
+});
