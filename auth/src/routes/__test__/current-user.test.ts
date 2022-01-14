13c5d8454038b25393dcb1123a385b05dcdc0043
Olusola Oseni <kenolusola@gmail.com>
2022-01-15 00:53:54 +0100

    test around current user functionality

---
+++ auth/src/routes/__test__/current-user.test.ts
@@ -0,0 +1,22 @@
+// used to fake request to the express app
+import request from "supertest";
+
+import { app } from "../../app";
+
+it("returns the details of the current user", async () => {
+  const cookie = await global.signup();
+  const response = await request(app)
+    .get("/api/users/currentuser")
+    .set("Cookie", cookie)
+    .send()
+    .expect(200);
+  expect(response.body.currentUser.email).toEqual("test@test.com");
+});
+
+it("responds with a null value if not authenticated", async () => {
+  const response = await request(app)
+    .get("/api/users/currentuser")
+    .send()
+    .expect(200);
+  expect(response.body.currentUser).toEqual(null);
+});
