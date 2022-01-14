d8be72cc00c88326783897fe2533f43b63fef9f4
Olusola Oseni <kenolusola@gmail.com>
2022-01-14 14:02:59 +0100

    refactor current user functionality

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,23 +1,15 @@
 import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import { currentUser } from "../middlewares/current-user";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req: Request, res: Response) => {
-  // if (!req.session || !req.session.jwt) {
-  //   return res.json({ currentUser: null });
-  // }
-
-  if (!req.session?.jwt) {
-    return res.status(401).json({ currentUser: null });
+router.get(
+  "/api/users/currentuser",
+  currentUser,
+  (req: Request, res: Response) => {
+    res.json({ currentUser: req.currentUser || null });
   }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.status(200).json({ currentUser: payload });
-  } catch (error) {
-    return res.status(401).json({ currentUser: null });
-  }
-});
+);
 
 export { router as currentUserRouter };
