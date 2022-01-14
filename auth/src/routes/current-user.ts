95e5f253473e9e9e069578d5ec82f046f8e7b315
Olusola Oseni <kenolusola@gmail.com>
2022-01-14 13:33:24 +0100

    implement current user functionality and signout

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,23 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-  res.json("Hi there");
+router.get("/api/users/currentuser", (req: Request, res: Response) => {
+  // if (!req.session || !req.session.jwt) {
+  //   return res.json({ currentUser: null });
+  // }
+
+  if (!req.session?.jwt) {
+    return res.status(401).json({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.status(200).json({ currentUser: payload });
+  } catch (error) {
+    return res.status(401).json({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
