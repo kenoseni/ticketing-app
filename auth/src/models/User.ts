f74f84b5049a0bd0538ad38ca2b9cda27321225c
Olusola Oseni <kenolusola@gmail.com>
2022-01-14 02:32:22 +0100

    create user model

---
+++ auth/src/models/User.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+// An interface that describes the properties
+// that are required to create a new user
+
+interface UserAttriButes {
+  email: string;
+  password: string;
+}
+
+// An interface that describes the properties
+// that a user model has
+interface UserModel extends mongoose.Model<UserDocument> {
+  build(attrs: UserAttriButes): UserDocument;
+}
+
+// An interface that describes the properties
+// that a user document has
+interface UserDocument extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+const userShema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userShema.statics.build = (attrs: UserAttriButes) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDocument, UserModel>("User", userShema);
+
+export { User };
