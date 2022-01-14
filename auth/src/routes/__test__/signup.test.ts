66940ca7f38e1b8be62c77bfa041bba6fda7a318
Olusola Oseni <kenolusola@gmail.com>
2022-01-14 22:44:25 +0100

    test suite for signup functionality

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -2,3 +2,43 @@
 import request from "supertest";
 
 import { app } from "../../app";
+
+it("returns a 201 on successful signup", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+});
+
+it("returns a 400 with an invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test",
+      password: "password",
+    })
+    .expect(400);
+});
+it("returns a 400 with an invalid password", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "pa",
+    })
+    .expect(400);
+});
+it("returns a 400 with missing email and password", async () => {
+  await request(app).post("/api/users/signup").send({
+    email: "tests@test.com",
+  });
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      password: "password",
+    })
+    .expect(400);
+});
