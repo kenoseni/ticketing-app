3c25a8db5d5a7dd57b92efdf2c9d009c753a6792
Olusola Oseni <kenolusola@gmail.com>
2022-01-21 11:03:42 +0100

    add a reuseable api client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,31 +1,17 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
   return <h1>Landing page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === "undefined") {
-    // we are on the server
-    //  kubectl get namespaces
-    // kubectl get services -n ingress-nginx
-    // use http://service-name.namespace.svc.cluster.local
-    const { data } = await axios
-      .get(
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-        {
-          headers: req.headers,
-        }
-      )
-      .catch((error) => console.log(error));
-    return data;
-  } else {
-    // we are on the browser
-    const { data } = await axios
-      .get("/api/users/currentuser")
-      .catch((error) => console.log(error));
-    return data;
-  }
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser").catch((error) => {
+    console.log(error);
+  });
+  return data;
 };
 
 export default LandingPage;
