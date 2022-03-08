4c46f3c2bcaac19d6d20437625ee171e76bd3078
Olusola Oseni <kenolusola@gmail.com>
2022-03-08 14:46:51 +0100

    add signin functionality

---
+++ client/pages/auth/signin.js
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import Router from "next/router";
+import useRequest from "../../hooks/use-request";
+
+const Signin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { doRequest, errors } = useRequest({
+    url: "/api/users/signin",
+    method: "post",
+    body: { email, password },
+    onSuccess: () => Router.push("/"),
+  });
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    doRequest();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Signin page</h1>
+      <div className="mb-3">
+        <label htmlFor="userEmail" className="form-label">
+          Email Address
+        </label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          className="form-control"
+          id="userEmail"
+          aria-describedby="emailHelp"
+        />
+        <div id="emailHelp" className="form-text">
+          We'll never share your email with anyone else.
+        </div>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="userPassword" className="form-label">
+          Password
+        </label>
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          className="form-control"
+          id="userPassword"
+          aria-describedby="passwordHelp"
+        />
+      </div>
+      {errors}
+      <button className="btn btn-primary">Sign In</button>
+    </form>
+  );
+};
+export default Signin;
