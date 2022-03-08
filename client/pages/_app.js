5491ba5c4550e0e03679178f3352ac9981742367
Olusola Oseni <kenolusola@gmail.com>
2022-03-08 17:22:08 +0100

    handling multiple getInitialProps

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,30 @@
 import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
 
-function TicketingApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function TicketingApp({ Component, pageProps, currentUser }) {
+  return (
+    <div>
+      {currentUser.email}
+      <Component {...pageProps} />
+    </div>
+  );
 }
+
+TicketingApp.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser").catch((error) => {
+    console.log(error);
+  });
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return {
+    pageProps,
+    // currentUser: data.currentUser,
+    ...data,
+  };
+};
 
 export default TicketingApp;
