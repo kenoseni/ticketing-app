c7a5bd80f807a6857c8c21b67baa69e3d93b2ecf
Olusola Oseni <kenolusola@gmail.com>
2022-03-10 10:15:19 +0100

    add infrastructure file for tickets microservice

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,9 +5,12 @@ const connectAndStart = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
+  }
   try {
-    await mongoose.connect("mongodb://auth-mongo-cluster-ip-srv:27017/auth");
-    console.log("Connected to Mongodb");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to auth Mongodb");
   } catch (error) {
     console.log(error);
   }
