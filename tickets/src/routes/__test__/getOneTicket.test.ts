022d646765fe07e9d8dc43bd65f62990239c38ab
Olusola Oseni <kenolusola@gmail.com>
2022-03-10 17:56:19 +0100

    update module and fix get one ticket test

--- tickets/src/routes/__test__/getOneTicket.test.ts
+++ tickets/src/routes/__test__/getOneTicket.test.ts
@@ -1,8 +1,10 @@
 import request from "supertest";
 import { app } from "../../app";
+import mongoose from "mongoose";
 
 it("returns a 404 if the ticket is not found", async () => {
-  const response = await request(app).get("/api/tickets/tetetevgeteg").send();
+  const id = new mongoose.Types.ObjectId().toHexString();
+  const response = await request(app).get(`/api/tickets/${id}`).send();
   expect(response.status).toEqual(404);
 });
 
