47a5a614b03a8769ac88938aa8a228032633cf6a
Olusola Oseni <kenolusola@gmail.com>
2022-03-10 15:51:34 +0100

    create ticket model

---
+++ tickets/src/models/ticket.ts
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+interface TicketAttriButes {
+  title: string;
+  price: number;
+  userId: string;
+}
+
+interface TicketDocument extends mongoose.Document {
+  title: string;
+  price: number;
+  userId: string;
+}
+
+interface TicketModel extends mongoose.Model<TicketDocument> {
+  build(attrs: TicketAttriButes): TicketDocument;
+}
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+    versionKey: false,
+  }
+);
+ticketSchema.statics.build = (attrs: TicketAttriButes) => {
+  return new Ticket(attrs);
+};
+
+const Ticket = mongoose.model<TicketDocument, TicketModel>(
+  "Ticket",
+  ticketSchema
+);
+
+export { Ticket };
