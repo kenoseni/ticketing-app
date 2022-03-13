db9e1b52504fe713bff4611438d4a77dd98de19d
Olusola Oseni <kenolusola@gmail.com>
2022-03-13 13:46:31 +0100

    add get one order implementation

--- orders/src/routes/getOneOrder.ts
+++ orders/src/routes/getOneOrder.ts
@@ -1,10 +1,27 @@
 import express, { Request, Response } from "express";
-import { NotFoundError } from "@sage-mode/common";
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from "@sage-mode/common";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.get("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+    const order = await Order.findById(orderId).populate("ticket");
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError("Not authorized to view this resource");
+    }
+    res.send(order);
+  }
+);
 
 export { router as getOneOrderRouter };
