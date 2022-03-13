1022742bb87643860b30b62e3d55ee9e9ec8c29e
Olusola Oseni <kenolusola@gmail.com>
2022-03-13 14:12:27 +0100

    add delete order implementation

--- orders/src/routes/deleteOrder.ts
+++ orders/src/routes/deleteOrder.ts
@@ -1,10 +1,29 @@
 import express, { Request, Response } from "express";
-import { NotFoundError } from "@sage-mode/common";
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from "@sage-mode/common";
+import { Order, OrderStatus } from "../models/order";
 
 const router = express.Router();
 
-router.delete("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.delete(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+    const order = await Order.findById(orderId);
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError("Not authorized to view this resource");
+    }
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+    res.send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
